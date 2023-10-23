64897e5b480877aac757fa1dd3cab6980f26f863
Nico Desktop <diaznicox96@gmail.com>
2023-10-23 18:48:43 -0300

    add AP folder with practice

---
+++ AP_nivelacion/src/Bucles.java
@@ -0,0 +1,77 @@
+import java.util.ArrayList;
+
+public class Bucles {
+    /**
+     * Pide dos números enteros, una condición que determina si salida son números pares o impares, y una condición
+     * que determina si la salida es en orden A-B o B-A, devuelve una cadena de texto separando por una coma el
+     * resultado.
+     * @param numberA
+     * @param numberB
+     * @param conditionIP true par, false impar
+     * @param conditionReverse true string invertido, false normal
+     * @return Una cadena de texto con la respuesta solicitada.
+     */
+    public static String BuclesPrimero(int numberA, int numberB, boolean conditionIP, boolean conditionReverse){
+        StringBuilder resultado = new StringBuilder();
+        ArrayList arrayResultado = new ArrayList();
+        if (conditionReverse){
+            for (int i = numberB; i >= numberA; i--){
+                if (conditionIP && i % 2 == 0){
+                    arrayResultado.add(i);
+                } else if (!conditionIP && i % 2 != 0){
+                    arrayResultado.add(i);
+                }
+            }
+        } else{
+            for (int i = numberA; i <= numberB; i++){
+                if (conditionIP && i % 2 == 0){
+                    arrayResultado.add(i);
+                } else if (!conditionIP && i % 2 != 0){
+                    arrayResultado.add(i);
+                }
+            }
+        }
+        for (int i = 0; i < arrayResultado.size(); i++){
+            resultado.append(i != arrayResultado.size()-1 ? (arrayResultado.get(i) + ", ") : arrayResultado.get(i));
+        }
+        return resultado.toString();
+    }
+
+    public static void TestBuclesPrimero(){
+        System.out.print("Numeros de 5 a 14 (sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(5, 14, true, false));
+
+        System.out.print("Numeros de 7 a 27 (sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(7, 27, true, false));
+
+        System.out.print("Numeros de 2 a 18 (sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(2, 18, true, false));
+
+        System.out.print("Numeros de 5 a 14 (inverso y sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(5, 14, true, true));
+
+        System.out.print("Numeros de 7 a 27 (inverso y sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(7, 27, true, true));
+
+        System.out.print("Numeros de 2 a 18 (inverso y sólo pares): ");
+        System.out.println(Bucles.BuclesPrimero(2, 18, true, true));
+
+        System.out.print("Numeros de 5 a 14 (sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(5, 14, false, false));
+
+        System.out.print("Numeros de 7 a 27 (sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(7, 27, false, false));
+
+        System.out.print("Numeros de 2 a 18 (sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(2, 18, false, false));
+
+        System.out.print("Numeros de 5 a 14 (inverso y sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(5, 14, false, true));
+
+        System.out.print("Numeros de 7 a 27 (inverso y sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(7, 27, false, true));
+
+        System.out.print("Numeros de 2 a 18 (inverso y sólo impares): ");
+        System.out.println(Bucles.BuclesPrimero(2, 18, false, true));
+    }
+}
---
+++ AP_nivelacion/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hola mundo!\nA continuación, ejercicios en java:\n\n");
+
+        System.out.println("Resultado de Ejercicio 1: ciclos, condicionales y arrayList:\n");
+        Bucles.TestBuclesPrimero();
+    }
+}
