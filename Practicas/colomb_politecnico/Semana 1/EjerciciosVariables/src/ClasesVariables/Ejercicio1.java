b2d6ac4ab69a5e370f82f5cd79203d828349e7b0
Nicolas Personal Desktop <diaznicox96@gmail.com>
2024-08-14 16:20:51 -0300

    (+) last exercise of first week, polytechnical

---
+++ Practicas/colomb_politecnico/Semana 1/EjerciciosVariables/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,17 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        String nombre = "Nicolas";
+        float x = 25.15f;
+        final double valorDolar = 959D;
+        float sieteDecimalesPi = 3.1415926f;
+        double quinceDecimalesE = 2.718281828459045;
+
+        System.out.println("Valor variable nombre: " + nombre);
+        System.out.println("Valor variable x: " + x);
+        System.out.println("Valor variable valorDolar: " + valorDolar);
+        System.out.println("Valor variable sieteDecimalesPi: " + sieteDecimalesPi);
+        System.out.println("Valor variable quinceDecimalesE: " + quinceDecimalesE);
+    }
+}
---
+++ Practicas/colomb_politecnico/Semana 1/EjerciciosVariables/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,21 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        Integer a = 8;
+        int b = (int) 'a';
+        String c = "Colombia";
+        char d = 'A';
+        float e = 74548f;
+        int unNumero;
+        String nombre_persona;
+        int edad;
+        String altura = "54";
+        double alturaReal = Double.parseDouble(altura);
+        int doble = 96;
+        a = 98;
+        int x = a;
+        int y = 25;
+        Integer g = y;
+    }
+}
